e1c7841ac732a019014ca7503833da028f3ed6b5
Aik <ikogweassurance85@gmail.com>
2022-08-19 09:03:59 +0100

    I added a test for the first input field to only read letters

--- aikscript.js
+++ aikscript.js
@@ -1,26 +1,35 @@
 const inputFields = document.getElementsByTagName("input");
+const errorMessage = document.querySelectorAll("#error_message");
 
 for (let i = 0; i < inputFields.length; i++) {
     inputFields[i].addEventListener("input", () => {
         inputFields[i].style.border = "1px solid hsl(278, 94%, 30%)";
         inputFields[i].style.outline = "1px solid hsl(278, 94%, 30%)";
-        if(!inputFields[i].value){
+        inputFields[i].style.textTransform = "none";
+        errorMessage[i].style.display = "none";
+        if (!inputFields[i].value) {
             inputFields[i].style.border = "1px solid  hsl(0, 100%, 66%)";
             inputFields[i].style.outline = "1px solid  hsl(0, 100%, 66%)";
-            const textElement = document.createElement("p");
-            const text = document.createTextNode("Can't be blank");
-            textElement.appendChild(text);
-            textElement.id = "error_message";
-            inputFields[i].parentElement.appendChild(textElement);
-            inputFields[i].addEventListener("input", () => {
-                textElement.style.display = "none";
-                inputFields[i].style.border = "1px solid hsl(278, 94%, 30%)";
-                inputFields[i].style.outline = "1px solid hsl(278, 94%, 30%)";
-                if(!inputFields[i].value){
+            errorMessage[i].style.display = "block";
+            errorMessage[i].innerHTML = "Can't be blank";
+        } else if (inputFields[i].value) {
+            if (i === 0) {
+                inputFields[i].style.textTransform = "capitalize";
+                if (/\d/.test(inputFields[i].value)) {
                     inputFields[i].style.border = "1px solid  hsl(0, 100%, 66%)";
                     inputFields[i].style.outline = "1px solid  hsl(0, 100%, 66%)";
+                    errorMessage[i].style.display = "block";
+                    errorMessage[i].innerHTML = "Wrong format, letters only";
                 }
-            });       
+            } 
+            // if (i === 1 || i === 2 || i === 3 || i === 4) {
+            //     if (/\d/.test(inputFields[i].value)) {
+            //         inputFields[i].style.border = "1px solid  hsl(0, 100%, 66%)";
+            //         inputFields[i].style.outline = "1px solid  hsl(0, 100%, 66%)";
+            //         errorMessage[i].style.display = "block";
+            //         errorMessage[i].innerHTML = "Wrong format, numbers only";
+            //     }
+            // }
         }
     });
 }
